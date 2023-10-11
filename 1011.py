ea760e0570d961f7ba963bfd136b5935679e9d7e
AcaiHi <C110110143@nkust.edu.tw>
2023-10-11 16:32:34 +0800

    Fix-contestTimeUpdate

---
+++ 1011.py
@@ -0,0 +1,34 @@
+# Chapter.4
+# 04. Distance Traveled
+
+speed = float(input('What is the speed of the vehicle in mph? '))
+hour = int(input('How many hours has it traveled? '))
+distance = 0
+print('Hour\t', 'Distance Traveled')
+print('-' * 30)
+
+for i in range(1, hour + 1):
+    
+    print(format(i, '1.0f'), '\t\t', format(i * speed, '6.1f'))
+    distance += i * speed
+# Chapter.4
+days = int(input('Enter the number of days: '))
+print('Day\t\t', 'Salary')
+print('-' * 25)
+total = 0
+for i in range(1, days + 1):
+    salary = 0.01 * 2 ** (i -1)
+    print(format(i, '3.0f'), '\t\t$', format(salary, '8.2f'))
+    total += salary
+print('The total pay at the end of the period is $', total)
+
+# Chapter.4
+# 08. Sum of Numbers
+
+number = float(input('Enter a positive number or enter a negative number '
+                     'if you want to end: '))
+total = 0
+while number > 0:
+    total += number
+    number = float(input('Enter a positive number: '))
+print('The sum is', total)
